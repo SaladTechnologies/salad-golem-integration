3261e9079c15d5c287bef1ef538209b129e8327a
Ben Hanna <benpaulhanna@gmail.com>
2025-11-13 16:32:33 -0500

    Adjusted logging config.

--- orchestrator/logger.ts
+++ orchestrator/logger.ts
@@ -3,7 +3,10 @@ import pino from 'pino';
 const logger = pino({
   level: 'info',
   transport: {
-    target: 'pino-pretty'
+    target: 'pino-pretty',
+    options: {
+      ignore: 'pid,hostname'
+    }
   }
 });
 
