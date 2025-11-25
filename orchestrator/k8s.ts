ed4acd75c4afd72897cfed46f4f66272a5ee8d94
Ben Hanna <benpaulhanna@gmail.com>
2025-11-25 15:56:53 -0500

    Optional kube config path.

--- orchestrator/k8s.ts
+++ orchestrator/k8s.ts
@@ -2,7 +2,13 @@ import { CoreV1Api, KubeConfig } from '@kubernetes/client-node';
 import config from 'config';
 
 const kc = new KubeConfig();
-kc.loadFromFile(config.get('kubeConfigPath')); // specify your kubeconfig path
+
+const kubeConfigPath = config.get<string>('kubeConfigPath');
+if (kubeConfigPath != null && kubeConfigPath !== '') {
+  kc.loadFromFile(kubeConfigPath); // specify your kubeconfig path
+} else {
+  kc.loadFromDefault();
+}
 
 export const k8sApi = kc.makeApiClient(CoreV1Api);
 export const k8sProviderNamespace = config.get<string>('k8sProviderNamespace'); // specify your namespace
