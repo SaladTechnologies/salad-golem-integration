236d52598be4b29526e085d6e95c80c93e7149f9
Ben Hanna <benpaulhanna@gmail.com>
2025-12-09 16:59:38 -0500

    WIP

--- orchestrator/requestor.ts
+++ orchestrator/requestor.ts
@@ -1,4 +1,4 @@
-import { V1PersistentVolumeClaim } from '@kubernetes/client-node';
+import { ApiException, CoreV1Api, V1PersistentVolumeClaim } from '@kubernetes/client-node';
 import { k8sApi } from './k8s.js';
 import { logger } from './logger.js';
 
@@ -132,6 +132,189 @@ const serviceManifest = {
     type: 'ClusterIP',
   },
 };
+
+export interface Requestor {
+  name: string;
+  environment: Record<string, string>;
+}
+
+function generateNames(name: string) {
+  return {
+    environmentName: `${name}-env`,
+    pvcName: `${name}-pvc`,
+    podName: name,
+    serviceName: `${name}-service`,
+  };
+}
+
+export async function provisionRequestor(k8sApi: CoreV1Api, namespace: string, requestor: Requestor) {
+  const names = generateNames(requestor.name);
+
+  const pvcManifest: V1PersistentVolumeClaim = {
+    apiVersion: 'v1',
+    kind: 'PersistentVolumeClaim',
+    metadata: {
+      name: names.pvcName,
+      namespace: k8sRequestorNamespace,
+    },
+    spec: {
+      accessModes: ['ReadWriteOnce'],
+      resources: {
+        requests: {
+          storage: '1Gi',
+        },
+      },
+    },
+  };
+
+  // Create PVC
+  try {
+    await k8sApi.createNamespacedPersistentVolumeClaim({ namespace, body: pvcManifest });
+    logger.info(`PersistentVolumeClaim ${names.pvcName} created`);
+  } catch (err) {
+    // Ignore if already exists
+    if (err instanceof ApiException && err.code === 409) {
+      logger.info(`PersistentVolumeClaim ${names.pvcName} already exists. Skipping creation.`);
+    }
+    else {
+      logger.error(err, `Error creating PersistentVolumeClaim ${names.pvcName}:`);
+    }
+  }
+
+  const podManifest = {
+    apiVersion: 'v1',
+    kind: 'Pod',
+    metadata: {
+      name: names.podName,
+      labels: {
+        'app.kubernetes.io/part-of': 'golem-network',
+        'app.kubernetes.io/version': 'v0.17.6-1',
+        'app.kubernetes.io/name': names.podName,
+      },
+    },
+    spec: {
+      initContainers: [
+        {
+          name: 'consent-writer',
+          image: 'busybox:1.37',
+          command: [
+            'sh',
+            '-c',
+            'echo "Stats allow" > /mnt/yagna/CONSENT && chown 1000:1000 /mnt/yagna/CONSENT && chmod 0664 /mnt/yagna/CONSENT',
+          ],
+          resources: {
+            limits: { cpu: '40m', memory: '128Mi' },
+            requests: { cpu: '10m', memory: '32Mi' },
+          },
+          volumeMounts: [
+            {
+              name: 'yagna-pvc',
+              mountPath: '/mnt/yagna',
+            },
+          ],
+        },
+      ],
+      containers: [
+        {
+          name: 'yagna',
+          image: 'saladtechnologies/golem-requestor:v0.17.6-1',
+          args: ['service', 'run'],
+          env: [
+            { name: 'POLYGON_MAX_FEE_PER_GAS', value: '1000' },
+            { name: 'CENTRAL_NET_HOST', value: 'polygongas.org:7999' },
+            { name: 'YA_NET_TYPE', value: 'central' },
+            { name: 'YAGNA_API_URL', value: 'http://0.0.0.0:7465' },
+            { name: 'YAGNA_AUTOCONF_APPKEY', value: names.podName },
+          ],
+          ports: [
+            { containerPort: 7465, name: 'http', protocol: 'TCP' },
+          ],
+          resources: {
+            limits: { cpu: '1', memory: '1Gi' },
+            requests: { cpu: '250m', memory: '256Mi' },
+          },
+          volumeMounts: [
+            {
+              name: 'yagna-pvc',
+              mountPath: '/home/ubuntu/.local/share/yagna/',
+            },
+          ],
+          livenessProbe: {
+            httpGet: {
+              path: '/version/get',
+              port: 7465,
+              httpHeaders: [
+                { name: 'Authorization', value: 'Bearer requestor-example' },
+              ],
+            },
+            initialDelaySeconds: 10,
+            periodSeconds: 10,
+            timeoutSeconds: 1,
+            successThreshold: 1,
+            failureThreshold: 3,
+          },
+        },
+      ],
+      volumes: [
+        {
+          name: names.pvcName,
+          persistentVolumeClaim: {
+            claimName: names.pvcName,
+          },
+        },
+      ],
+      securityContext: {
+        fsGroup: 1000,
+      },
+      terminationGracePeriodSeconds: 60,
+    },
+  };
+
+  // Create Pod
+  try {
+    await k8sApi.createNamespacedPod({ namespace, body: podManifest });
+    logger.info(`Pod ${names.podName} created`);
+  } catch (err) {
+    logger.error(err, `Error creating pod ${names.podName}:`);
+  }
+
+  const serviceManifest = {
+    apiVersion: 'v1',
+    kind: 'Service',
+    metadata: {
+      name: names.serviceName,
+    },
+    spec: {
+      selector: {
+        'app.kubernetes.io/part-of': 'golem-network',
+        'app.kubernetes.io/name': names.podName,
+      },
+      ports: [
+        {
+          name: 'http',
+          port: 7465,
+          targetPort: 7465,
+          protocol: 'TCP',
+        },
+      ],
+      type: 'ClusterIP',
+    },
+  };
+
+  // Create Service
+  try {
+    await k8sApi.createNamespacedService({ namespace, body: serviceManifest });
+    logger.info(`Service ${names.serviceName} created`);
+  } catch (err) {
+    // Ignore if already exists
+    if (err instanceof ApiException && err.code === 409) {
+      logger.info(`Service ${names.serviceName} already exists. Skipping creation.`);
+    }
+    else {
+      logger.error(err, `Error creating service ${names.serviceName}:`);
+    }
+  }
+}
 
 async function main() {
   // Create PVC
