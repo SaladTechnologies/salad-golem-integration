8e0b639102c7fa9500c1053781677ca8153e5e41
Ben Hanna <benpaulhanna@gmail.com>
2026-01-08 16:32:22 -0500

    Update script to take commit hash and truncate it.

--- scripts/build-and-push-images.ps1
+++ scripts/build-and-push-images.ps1
@@ -1,7 +1,13 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string]$Tag
+    [string]$CommitHash
 )
+
+if (-not $CommitHash -or $CommitHash.Length -lt 7) {
+    Write-Host "Commit hash must be at least 7 characters." -ForegroundColor Red
+    exit 1
+}
+$Tag = $CommitHash.Substring(0,7)
 
 $folders = @("plan-importer", "orchestrator", "price-importer")
 $dockerRepo = "saladtechnologies" # Replace with your Docker Hub repo or registry
